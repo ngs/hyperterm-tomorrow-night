6d6a21588b642891122033a6c50528adb5cad917
Atsushi Nagase <a@ngs.io>
2016-07-17 12:48:08 +0900

    Initial import

---
+++ Readme.md
@@ -0,0 +1,14 @@
+hyperterm-tomorrow-night
+========================
+
+[Hyperterm] theme based on [Tomorrow Night theme]
+
+![Screenshot](screenshot.png)
+
+How to use
+----------
+
+Add `hyperterm-tomorrow-night` to `plugins` in ``~/.hyperterm.js`.
+
+[Hyperterm]: https://hyperterm.org/
+[Tomorrow Night theme]: https://github.com/chriskempson/tomorrow-theme#tomorrow-night
---
+++ index.js
@@ -0,0 +1,33 @@
+// Tomorrow Night Theme
+
+exports.middleware = (store) => (next) => (action) => {
+  switch (action.type) {
+    case 'CONFIG_LOAD':
+    case 'CONFIG_RELOAD':
+      action.config.foregroundColor = '#c5c8c6';
+      action.config.backgroundColor = '#1d1f21';
+      action.config.cursorColor = '#373b41';
+      action.config.colors = [
+        "#1d1f21",
+        "#cc6666",
+        "#b5bd68",
+        "#f0c674",
+        "#81a2be",
+        "#b57880",
+        "#8abeb7",
+        "#282a2e",
+        "#969896",
+        "#cc6666",
+        "#b5bd68",
+        "#f0c674",
+        "#81a2be",
+        "#b57880",
+        "#8abeb7",
+        "#ffffff",
+        "#c5c8c6"
+      ];
+      action.config.css = '.title_1i8co95 { color: #c5c8c6 !important }'
+  }
+  next(action)
+}
+
